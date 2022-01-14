aba7cadf3b9be51ac228f45a9ae1b2c05eb8c981
camdyn-dev <89356978+camdyn-dev@users.noreply.github.com>
2022-01-13 21:01:00 -0800

    Added files

---
+++ index.js
@@ -0,0 +1,65 @@
+//trying to make the code for a web calculator
+
+
+//works pretty well, need to work on a few things tho
+//1: make sure division by zero doesn't fuck the whole thing
+//2: add an option where, if the next button pressed is not another operator, it starts a new equation (probably an //if statement inside the nums loop)
+//3: make sure the eval is secure with regex or some shit i dunno
+//4: CSS B R U H
+const nums = document.querySelectorAll(".num")
+const operators = document.querySelectorAll(".operator")
+const evaluate = document.querySelector(".evaluate")
+const removeOne = document.querySelector(".removeOne")
+const removeAll = document.querySelector(".removeAll")
+
+const display = document.querySelector("#display")
+const previousVal = document.querySelector("#previousVal")
+let equation = ""
+let solution = ""
+
+
+
+//adds functionality to the buttons on the thingerooni waannani with da binderooni and without THIS
+for (let i = 0; i < nums.length; i++) {
+    nums[i].addEventListener("click", function (e) {
+        solution = ""
+        equation += e.target.value
+        display.textContent = equation
+    }.bind(nums[i]))
+}
+
+//binds da operator buttons
+for (let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener("click", function (e) {
+        if (solution) {
+            equation += solution
+            solution = ""
+        }
+        equation += e.target.value
+        display.textContent = equation
+    }.bind(operators[i]))
+}
+
+
+
+evaluate.addEventListener("click", () => {
+    solution = eval(equation)
+    previousVal.textContent = equation
+    display.textContent = solution
+    equation = ""
+
+
+})
+
+removeOne.addEventListener("click", () => {
+    equation = equation.slice(0, -1)
+    display.textContent = equation
+})
+
+removeAll.addEventListener("click", () => {
+    equation = ""
+    solution = ""
+    display.textContent = equation
+    previousVal.textContent = solution
+})
+
